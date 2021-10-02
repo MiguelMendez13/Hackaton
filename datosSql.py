56f28d75836c63e88c6a860d186bd27c2fe993dc
MiguelMendez13 <62361525+MiguelMendez13@users.noreply.github.com>
2021-10-02 13:52:52 -0500

    Update datosSql.py

--- datosSql.py
+++ datosSql.py
@@ -29,23 +29,69 @@ for x in range(0,150):
 
 
 embarazoT=[0,0,0]
-diabetisT=[]
-epocT=[]
-asmaT=[0,0,0]
-hipertensionT=[]
-cardioT=[]
-obesidadT=[]
-tabaquismoT=[]
+diabetisT = [0,0,0]
+epocT = [0,0,0]
+asmaT = [0, 0, 0]
+hipertensionT = [0, 0, 0]
+cardioT = [0,0,0]
+obesidadT = [0,0,0]
+tabaquismoT = [0,0,0]
+muertesT= [0,0]
+fechaM = "9999-99-99"
 
 
 
 for x in baseLeerLinea:
 	cortar=x.split(',')
 	sexo=cortar[5]
+	hipertension = cortar[24]
+	cardio = cortar[26]
+	obesidad = cortar[27]
+	tabaquismo = cortar[29]
+	epoc = cortar[21]
+	muertes = cortar[12]
+	diabetis = cortar[20]
+
 	if sexo=="1":sexoT[0]+=1
 	elif  sexo=="2":sexoT[1]+=1
 	elif sexo=="99":sexoT[2]+=1
 	else:pass
+
+	if hipertension == "1": hipertensionT[0] += 1
+	elif hipertension == "2": hipertensionT[1] += 1
+	elif hipertension == "99": hipertensionT[2] += 1
+	else: pass
+
+	if cardio == "1": cardioT[0] += 1
+	elif cardio == "2": cardioT[1] += 1
+	elif cardio == "99": cardioT[2] += 1
+	else: pass
+	
+	if obesidad == "1": obesidadT[0] += 1
+	elif obesidad == "2": obesidadT[1] += 1
+	elif obesidad == "99": obesidadT[2] += 1
+	else: pass
+
+	if tabaquismo == "1": tabaquismoT[0] += 1
+	elif tabaquismo == "2": tabaquismoT[1] += 1
+	elif tabaquismo == "99": tabaquismoT[2] += 1
+	else: pass
+
+	if epoc == "1": epocT[0] += 1
+	elif epoc == "2": epocT[1] += 1
+	elif epoc == "99": epocT[2] += 1
+	else: pass
+
+	if diabetis == "1": diabetisT[0] += 1
+	elif diabetis == "2": diabetisT[1] += 1
+	elif diabetis == "99": diabetisT[2] += 1
+	else: pass
+
+	if muertes == fechaM: muertesT[0] += 1
+	elif muertes != fechaM : muertesT[1] += 1
+	else: pass
+
+	
 	#-------------
 	embarazo=cortar[17]
 	if embarazo=="1":embarazoT[0]+=1
@@ -60,36 +106,43 @@ for x in baseLeerLinea:
 	else:asmaT[2]+=1
 
 	entidadResT[int(cortar[7].replace('"',""))]+=1
-	edadT[int(cortar[15])]+=1
 	try:
 		municipioResT[int(cortar[7].replace('"',""))][int(cortar[8].replace('"',""))]+=1
 	except Exception as e:
 		municipioResT[int(cortar[7].replace('"',""))][999]+=1
+	#-------------
+	edadT[int(cortar[15])]+=1
 
 
 
 
 
-
-"""
-
-	diabetisT.append(x.split(',')[20])
-	epocT.append(x.split(',')[21])
-
-
-	hipertensionT.append(x.split(',')[24])
-	cardioT.append(x.split(',')[26])
-	obesidadT.append(x.split(',')[27])
-	tabaquismoT.append(x.split(',')[29])"""
-
+print("\n\nsexoT\n")
 print(sexoT)
-print("\n\n")
+print("\n\nentidadResT\n")
 print(entidadResT)
-print("\n\n")
+print("\n\nedadT\n")
 print(edadT)
-print("\n\n")
+print("\n\nasmaT\n")
 print (asmaT)
-print("\n\n")
+print("\n\nembarazoT\n")
 print (embarazoT)
-print("\n\n")
+print("\n\nmunicipioResT\n")
 print (municipioResT)
+
+print("\n\ndiabetisT\n")
+print(diabetisT)
+print("\n\nepocT\n")
+print(epocT)
+print("\n\nasmaT\n")
+print(asmaT)
+print("\n\nhipertensionT\n")
+print(hipertensionT)
+print("\n\ncardioT\n")
+print(cardioT)
+print("\n\nobesidadT\n")
+print(obesidadT)
+print("\n\ntabaquismoT\n")
+print(tabaquismoT)
+print("\n\nmuertesT\n")
+print(muertesT)
